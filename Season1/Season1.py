93d43c10aed2c9129231cc6bd0937ed50b31374c
Sergio Padilla <s.padilla@rezoloco.com>
2016-12-16 12:49:11 +0100

    forgive

--- Season1/Season1.py
+++ Season1/Season1.py
@@ -11,15 +11,15 @@ if __name__ == '__main__':
     axis_x = []
     axis_y = []
     for planet in planets:
-        for i in range(0, 2000):
-            print('Momento angular NR: ', planet.angular_moment_newton_raphson(i))
-            print('Momento angular Bessel: ', planet.angular_moment_bessel(i))
-            print('u NR: ', planet.get_u_newton_raphson(i))
-            print('u Bessel: ', planet.get_u_bessel(i))
+        print('Momento angular NR: ', planet.angular_moment_newton_raphson(50))
+        print('Momento angular Bessel: ', planet.angular_moment_bessel(50))
+        print('u NR: ', planet.get_u_newton_raphson(50))
+        print('u Bessel: ', planet.get_u_bessel(50))
+        for i in range(0, int(planet.period+1)):
             position = planet.get_pos_newton_raphson(i)
             axis_x.append(position[0])
             axis_y.append(position[1])
 
     plt.plot(axis_x, axis_y, 'r.')
-    plt.axis([-20, 20, -20, 20])
+    plt.axis([-40, 40, -40, 40])
     plt.show()
