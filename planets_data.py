f3b57abd77955fc552119c89fdf091e0ec1e1d9b
Sergio Padilla <splsergiopadilla@gmail.com>
2017-01-20 01:15:22 +0100

    Update planets_data.py

--- planets_data.py
+++ planets_data.py
@@ -4,16 +4,16 @@ __authors__ = 'Sergio Padilla / Marina Estévez / Irene Ocaña'
 """
 planets = {
     'Mercurio': {
-                'a': 0.39,
-                'epsilon': 0.2056,
+                'a': 0.387,
+                'epsilon': 0.206,
                 'p': 87.66,
                 'i': 7,
                 'capital_omega': 47.14,
                 'omega_bar': 75.9
                 },
     'Venus': {
-                'a': 0.72,
-                'epsilon': 0.0068,
+                'a': 0.723,
+                'epsilon': 0.007,
                 'p': 226.459,
                 'i': 3.59,
                 'capital_omega': 75.78,
@@ -21,47 +21,47 @@ planets = {
             },
     'Tierra': {
                 'a': 1,
-                'epsilon': 0.0167,
+                'epsilon': 0.017,
                 'p': 365.26,
                 'i': 0,
                 'capital_omega': 0,
                 'omega_bar': 101.22
             },
     'Marte': {
-                'a': 1.52,
-                'epsilon': 0.0934,
+                'a': 1.524,
+                'epsilon': 0.093,
                 'p': 686.68,
                 'i': 1.85,
                 'capital_omega': 48.78,
                 'omega_bar': 334.22
             },
     'Jupiter': {
-                'a': 5.2,
-                'epsilon': 0.0483,
+                'a': 5.203,
+                'epsilon': 0.048,
                 'p': 4331.936,
                 'i': 1.31,
                 'capital_omega': 99.44,
                 'omega_bar': 12.72
             },
     'Saturno': {
-                'a': 9.54,
-                'epsilon': 0.0560,
+                'a': 9.546,
+                'epsilon': 0.056,
                 'p': 10760.442,
                 'i': 2.5,
                 'capital_omega': 112.79,
                 'omega_bar': 91.09
             },
     'Urano': {
-                'a': 19.18,
-                'epsilon': 0.0461,
+                'a': 19.20,
+                'epsilon': 0.047,
                 'p': 30685.157,
                 'i': 0.77,
                 'capital_omega': 73.48,
                 'omega_bar': 169.05
             },
     'Neptuno': {
-                'a': 30.06,
-                'epsilon': 0.0097,
+                'a': 30.09,
+                'epsilon': 0.009,
                 'p': 60194.19,
                 'i': 1.78,
                 'capital_omega': 130.68,
